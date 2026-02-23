8270aad28062a04c76710c78db6c69fa6460352b
Zakhar Puchka <puchkazahar@gmail.com>
2026-02-23 13:32:26 +0100

    Add weekday mapping and lesson retrieval function

--- schedule.py
+++ schedule.py
@@ -66,3 +66,19 @@ SCHEDULE = [
         "room": "313",
     },
 ]
+
+
+WEEKDAY_TO_INDEX = {
+    "Monday": 0,
+    "Tuesday": 1,
+    "Wednesday": 2,
+    "Thursday": 3,
+    "Friday": 4,
+    "Saturday": 5,
+    "Sunday": 6,
+}
+
+
+def get_lessons_for_day(weekday_index: int) -> list:
+    weekday_name = list(WEEKDAY_TO_INDEX.keys())[weekday_index]
+    return [lesson for lesson in SCHEDULE if lesson["weekday"] == weekday_name]
